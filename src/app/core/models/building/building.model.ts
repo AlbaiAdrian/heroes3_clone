20dbedacb444254b7f8ad7cee9df03a9e1e21c3c
Albai Adrian <albaiadrian.com>
2026-01-30 20:11:56 +0200

    Updated building model

--- src/app/core/models/building/building.model.ts
+++ src/app/core/models/building/building.model.ts
@@ -1,4 +1,5 @@
 // core/models/building/building.model.ts
+import { CreatureLevel } from '../creature/creature-level.enum';
 import { Resource } from '../player/resources.model';
 
 export interface Building {
@@ -6,7 +7,12 @@ export interface Building {
   cost: Resource[];
   /** Array of prerequisite building names that must be constructed before this building */
   prerequisiteBuildings: string[];
-  /** Reference to the creature type ID that this building produces (for dwelling buildings) */
-  creatureTypeId?: string;
+  
+  /** 
+   * Reference to the creature level that this building produces (for dwelling buildings) 
+   * Since the building may not always produce creatures, this field is optional
+   * We do not provide the faction here, as it is implied by the town type
+  */
+  creatureLevel?: CreatureLevel;
   growth?: number;
 }
